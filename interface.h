cab6510eb514453d93c0ef247b4224dd3aec90f3
dd01001010 <d01001010@hotmail.com>
2019-06-26 15:19:56 -0700

    commit for BST

---
+++ interface.h
@@ -0,0 +1,31 @@
+#ifndef Interface_Header
+#define Interface_Header
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class BST{
+    private:
+
+        struct node
+        {
+            /* data */
+            int key;
+            node* left;
+            node* right;
+        };
+        
+        node* root;
+        void addLeafPriv(int key, node* ptr);
+        void printInOrderPriv(node* ptr);
+    public:
+    
+        void tstPrint();
+        BST();
+        node* CreateLeaf(int key);
+        void addLeaf(int key);
+        void printInOrder();
+
+};
+#endif
